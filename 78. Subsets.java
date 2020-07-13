866a427776c7dc8ab9aa7acc66bcc75ad71bef7c
aaronyan <aaronyan@tencent.com>
2020-07-13 20:20:37 +0800

    optimize 78

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -19,25 +19,45 @@ Output:
 ]
 */
 import java.util.Arrays;
+import java.util.List;
 class Solution {
+
+    //递归
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     if(nums.length == 0){
+    //         List<List<Integer>> ans = new ArrayList<>();
+    //         List<Integer> subset = new ArrayList<>();
+    //         ans.add(subset);
+    //         return ans;
+    //     }
+    //     int n = nums.length;
+    //     int i = nums[n - 1];
+    //     int[] preNums = Arrays.copyOfRange(nums, 0, n - 1); 
+    //     List<List<Integer>> preSubsets = subsets(preNums);
+    //     List<List<Integer>> ans = new ArrayList<>(preSubsets);
+    //     for(List<Integer> subset : preSubsets)
+    //     {
+    //         List<Integer> newSubset = new ArrayList<Integer>(subset);
+    //         newSubset.add(i);
+    //         ans.add(newSubset);
+    //     }
+    //     return ans;
+    // }
+
+    //迭代
     public List<List<Integer>> subsets(int[] nums) {
-        if(nums.length == 0){
-            List<List<Integer>> ans = new ArrayList<>();
-            List<Integer> subset = new ArrayList<>();
-            ans.add(subset);
-            return ans;
-        }
-        int n = nums.length;
-        int i = nums[n - 1];
-        int[] preNums = Arrays.copyOfRange(nums, 0, n - 1); 
-        List<List<Integer>> preSubsets = subsets(preNums);
-        List<List<Integer>> ans = new ArrayList<>(preSubsets);
-        for(List<Integer> subset : preSubsets)
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());
+        for(int i : nums)
         {
-            List<Integer> newSubset = new ArrayList<Integer>(subset);
-            newSubset.add(i);
-            ans.add(newSubset);
+            int size = subsets.size();
+            for(int j = 0; j < size; j++)
+            {
+                List<Integer> newSubset = new ArrayList<Integer>(subset.get(j));
+                newSubset.add(i);
+                subsets.add(newSubset);
+            }
         }
-        return ans;
+        return subsets;
     }
 }
