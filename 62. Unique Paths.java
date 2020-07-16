1990dedd2b17732ea812a9810706fff71ea1ec01
aaronyan <aaronyan@tencent.com>
2020-07-16 17:21:06 +0800

    62

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -1,14 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dp = new int[m][n];
         //init
-        for(int i = 0; i < m + 1; i++)dp[i][0] = 0;
-        for(int j = 0; j < n + 1; j++)dp[0][j] = 0;
-        for(int i = 1; i < m + 1; i++){
-            for(int j = 1; j < n + 1; j++){
+        for(int i = 0; i < m; i++)dp[i][0] = 1;
+        for(int j = 0; j < n; j++)dp[0][j] = 1;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m + 1][n + 1];
+        return dp[m - 1][n - 1];
     }
 }
