321a4c07e9898487586b0d232154b30aa90b4911
aaronyan <yanqifa21@gmail.com>
2020-07-24 21:07:20 +0800

    add coding interview

--- coding-interviews/32 - II. 从上到下打印二叉树 II.java
+++ coding-interviews/32 - II. 从上到下打印二叉树 II.java
@@ -1,3 +1,33 @@
-public class 32 - II. 从上到下打印二叉树 II {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode pRoot) {
+    List<List<Integer>> ans = new ArrayList<>();
+        if(pRoot == null) return ans;
+        LinkedList<TreeNode> queue= new LinkedList<>();
+        queue.add(pRoot);
+        
+        while(!queue.isEmpty()){
+            ArrayList<TreeNode> lineNode = new ArrayList<>();
+            ArrayList<Integer> lineVal = new ArrayList<>();
+            do{
+                TreeNode node = queue.pop();
+                lineNode.add(node);
+                lineVal.add(node.val);
+            }while(!queue.isEmpty());
+            for(TreeNode node : lineNode){
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            ans.add(lineVal);   
+        }
+        return ans;
+    }
 }
