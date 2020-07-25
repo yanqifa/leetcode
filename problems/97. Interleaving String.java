402584064b353ae15119234d1aec9bf4b1e2bcc7
aaronyan <yanqifa21@gmail.com>
2020-07-25 21:14:53 +0800

    update

--- problems/97. Interleaving String.java
+++ problems/97. Interleaving String.java
@@ -1,8 +1,29 @@
 class Solution {
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        //todo
-        return false;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        if(len1 + len2 != len3) return false;
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+        //init
+        for(int i = 1; i < len1 + 1; i++){
+            if(s1.charAt(i - 1) == s3.charAt(i - 1)) dp[i][0] = true;
+            else break;
+        }
+
+        for(int j = 1; j < len2 + 1; j++){
+            if(s2.charAt(j - 1) == s3.charAt(j - 1)) dp[0][j] = true;
+            else break;
+        }
+
+        for(int i = 1; i < len1 + 1; i++){
+            for(int j = 1; j < len2 + 1; j++){
+                if((dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1) ) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j -1)))dp[i][j] = true;
+            }
+        }
+        return dp[len1][len2];
     }
     
     //TLE
